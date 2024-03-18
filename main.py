cf3ed546f2453da5dfcdac7c9ac900d9170394ea
vgoroshenko <vgoroshenko1@gmail.com>
2024-03-18 18:26:15 +0600

    fix distance config

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import telebot
 from tornado.websocket import websocket_connect
 from tornado.ioloop import IOLoop
 from tornado import gen
-from geopy.distance import geodesic
+from geopy import distance
 import geocoder
 import logging
 import json
@@ -17,12 +17,13 @@ import sys
 echo_uri = 'wss://www.seismicportal.eu/standing_order/websocket'
 # echo_uri = 'wss://localhost'
 PING_INTERVAL = 15
-current_location = [42.87, 74.59] #geocoder.ip('me')
+current_location = (42.87, 74.59) #geocoder.ip('me')
 tmp_info = {'unid': ''}
 print(current_location)
 #bot = TgBot()
 
-TOKEN = ''
+TOKEN = ""
+
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 messages = [
     {"role": "system", "content": "You are a helpful assistant."},
@@ -40,7 +41,7 @@ def send_welcome(message):
             txt.write('\n' + str(message.chat.id))
 
 @bot.message_handler(commands=['worked'])
-def bot_worked_message(self):
+def bot_worked_message():
     for i in users:
         if i.isdigit():
             chat_id = i
@@ -65,8 +66,8 @@ def myprocessing(message):
         data = json.loads(message)
         shake_info = data['data']['properties']
         shake_info['action'] = data['action']
-        shake_location = [shake_info['lat'], shake_info['lon']]
-        distance = geodesic(shake_location, current_location).kilometers
+        shake_location = (shake_info['lat'], shake_info['lon'])
+        my_distance = distance.distance(shake_location, current_location).km
         shake_info['time'] = re.sub(r'T', ' ', re.sub(r'\..*$', '', shake_info['time']))
         shake_info['cur_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         shake_info['time'] = (datetime.strptime(shake_info['time'], "%Y-%m-%d %H:%M:%S") + timedelta(hours=6)).strftime(
@@ -90,10 +91,10 @@ def myprocessing(message):
                 # Alarm if mag > 4
                 if shake_info['mag'] > 4:
                     send_shake_message(msg.format(**shake_info), disable_notification)
-                # Alarm if distance < 400
-                if distance <= 400:
+                # Alarm if my_distance < 400
+                if my_distance <= 400:
                     for i in range(7):
-                        send_shake_message('‼️‼️‼️\n' + msg.format(**shake_info) + ' DISTANCE: ' + str(distance), False)
+                        send_shake_message('‼️‼️‼️\n' + msg.format(**shake_info) + ' DISTANCE: ' + str(my_distance), False)
 
             tmp_info.update(shake_info)
 
